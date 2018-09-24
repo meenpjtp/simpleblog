fbace2564284cce9e8cad761bb7da3b08fc69321
5710451398 <pinjuta.t@ku.th>
2018-09-24 16:43:56 +0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+# call localhost:3000 display index.html
+root 'posts#index', as: 'home'
+
+# call localhost:3000/about display about.html
+get 'about' => 'pages#about', as: 'about'
+
+resources :posts
+
+end
